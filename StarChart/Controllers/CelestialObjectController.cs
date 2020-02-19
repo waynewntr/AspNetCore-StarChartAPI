90ee5a9332cb5ccdc764ffa20421be7b4d969137
waynewntr <waynewntr@gmail.com>
2020-02-19 18:22:34 +0200

    Update CelestialObjectController.cs

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -17,7 +17,7 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
-        [HttpGet("{int : Id}", Name = "GetById")]
+        [HttpGet("{Id:int}", Name = "GetById")]
         public IActionResult GetById(int Id)
         {
             var celestialObject = _context.CelestialObjects.Find(Id);
